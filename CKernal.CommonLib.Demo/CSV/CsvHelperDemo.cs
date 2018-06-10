73fd4ad8e927faecfab6d8576518d631327116d0
CKernal <523613009@qq.com>
2018-06-10 10:49:35 +0800

    1.添加CsvHelperDemo测试项目。

---
+++ CKernal.CommonLib.Demo/CSV/CsvHelperDemo.cs
@@ -0,0 +1,37 @@
+﻿using CKernal.CommonLib.CSV;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKernal.CommonLib.Demo.CSV
+{
+    public class CsvHelperDemo
+    {
+        public void CsvHelperReadWriteTest()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Restart();
+            DataTable dt = CsvHelper.OpenCSV(@"D:\barcode\Fixture1.csv");
+            sw.Stop();
+            Console.WriteLine(string.Format("OpenCSV: {0}", sw.Elapsed.TotalMilliseconds));
+
+            if (dt != null)
+            {
+                sw.Restart();
+                var barcode = dt.AsEnumerable()
+                        .Select(r => r.Field<string>("Barcode"))
+                        .ToArray() ;
+                sw.Stop();
+                Console.WriteLine(string.Format("Select: {0}", sw.Elapsed.TotalMilliseconds));
+
+                sw.Restart();
+                CsvHelper.SaveCSV(@"D:\barcode\Fixture11111111.csv", dt);
+                sw.Stop();
+                Console.WriteLine(string.Format("SaveCSV: {0}", sw.Elapsed.TotalMilliseconds));
+            }
+        }
+    }
+}
