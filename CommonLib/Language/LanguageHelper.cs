727a758d6f1a5c40e71c2fab7cdc296657330df9
CKernal <523613009@qq.com>
2018-07-31 00:16:16 +0800

    1.两个重载语言函数合并。

--- CommonLib/Language/LanguageHelper.cs
+++ CommonLib/Language/LanguageHelper.cs
@@ -9,20 +9,8 @@ namespace CommonLib.Language
     public class LanguageHelper
     {
         private static ConcurrentDictionary<Control, ResourceManager> m_controlResourceDictionary = new ConcurrentDictionary<Control, ResourceManager>();
-        public static string GetControlResourcesString(Control control, string key)
-        {
-            if (string.IsNullOrEmpty(key)) return string.Empty;
 
-            if (!m_controlResourceDictionary.ContainsKey(control))
-            {
-                ResourceManager resource = new ResourceManager(control.GetType());
-                m_controlResourceDictionary.AddOrUpdate(control, resource, (oldkey, oldvalue) => resource);
-            }
-            
-            return m_controlResourceDictionary[control].GetString(key);
-        }
-
-        public static string GetControlResourcesString(Control control, string key, CultureInfo culture)
+        public static string GetControlResourcesString(Control control, string key, CultureInfo culture = null)
         {
             if (string.IsNullOrEmpty(key)) return string.Empty;
 
@@ -32,6 +20,10 @@ namespace CommonLib.Language
                 m_controlResourceDictionary.AddOrUpdate(control, resource, (oldkey, oldvalue) => resource);
             }
 
+            if (culture == null)
+            {
+                return m_controlResourceDictionary[control].GetString(key);
+            }
             return m_controlResourceDictionary[control].GetString(key, culture);
         }
     }
